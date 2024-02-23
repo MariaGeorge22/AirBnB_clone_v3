a2f83e077c8d7c4e31519e957a7eb10db00db882
DJ-Man-2099 <davidmesak@hotmail.com>
2024-02-23 23:12:10 +0200

    Optional Task 1

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -12,7 +12,7 @@ from models.user import User
 
 @app_views.route('/places/<place_id>/amenities/<amenity_id>',
                  methods=['POST'], strict_slashes=False)
-def post_amenity(place_id, amenity_id):
+def post_place_amenity(place_id, amenity_id):
     """Creates a Amenity"""
     new_place = storage.get(Place, place_id)
     if not new_place:
@@ -33,7 +33,7 @@ def post_amenity(place_id, amenity_id):
 
 @app_views.route('/places/<place_id>/amenities',
                  methods=['GET'], strict_slashes=False)
-def get_amenities(place_id):
+def get_place_amenities(place_id):
     """Retrieves the list of all Amenity objects of a Place"""
     place = storage.get(Place, place_id)
     if not place:
@@ -43,7 +43,7 @@ def get_amenities(place_id):
 
 @app_views.route('/places/<place_id>/amenities/<amenity_id>',
                  methods=['DELETE'], strict_slashes=False)
-def delete_amenity(place_id, amenity_id):
+def delete_place_amenity(place_id, amenity_id):
     """Deletes a Amenity object"""
     amenity = storage.get(Amenity, amenity_id)
     place = storage.get(Place, place_id)
