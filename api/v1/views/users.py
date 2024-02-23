7b3a00846adb1768b987442fc2f0dcfe7733a63b
DJ-Man-2099 <davidmesak@hotmail.com>
2024-02-23 20:59:26 +0200

    Task 8

---
+++ api/v1/views/users.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+"""view for State objects"""
+
+from flask import abort, jsonify, request
+from api.v1.views import app_views
+from models import storage
+from models.user import User
+
+
+@app_views.route('/users',
+                 methods=['POST'], strict_slashes=False)
+def post_user():
+    """Creates an User"""
+    try:
+        data = request.get_json()
+    except:
+        return {'error': 'Not a JSON'}, 400
+    email = data.get('email')
+    if not email:
+        return {'error': 'Missing email'}, 400
+    password = data.get('password')
+    if not password:
+        return {'error': 'Missing password'}, 400
+    new_user = User(**data)
+    storage.new(new_user)
+    storage.save()
+    return new_user.to_dict(), 201
+
+
+@app_views.route('/users/<user_id>',
+                 methods=['PUT'], strict_slashes=False)
+def put_user(user_id):
+    """Retrieves the list of all State objects"""
+    new_user = storage.get(User, user_id)
+    if not new_user:
+        abort(404)
+    try:
+        data = request.get_json()
+    except:
+        return {'error': 'Not a JSON'}, 400
+    for key, value in data.items():
+        if key not in ['id', 'created_at',
+                       'updated_at', 'email']:
+            setattr(new_user, key, value)
+    storage.save()
+    return new_user.to_dict(), 200
+
+
+@app_views.route('/users',
+                 methods=['GET'], strict_slashes=False)
+def get_users():
+    """Retrieves the list of all City objects of a State"""
+    return jsonify(list(map(lambda x: x.to_dict(),
+                            storage.all(User).values())))
+
+
+@app_views.route('/users/<user_id>',
+                 methods=['GET'], strict_slashes=False)
+def get_user(user_id):
+    """Retrieves a User object"""
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+    return user.to_dict()
+
+
+@app_views.route('/users/<user_id>',
+                 methods=['DELETE'], strict_slashes=False)
+def delete_user(user_id):
+    """Retrieves a User object"""
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+    storage.delete(storage.get(User, user_id))
+    storage.save()
+    return {}, 200
