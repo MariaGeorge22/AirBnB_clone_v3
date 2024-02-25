7b87229fd4fe48f1d0e2e74f3cee685b96faacc0
DJ-Man-2099 <davidmesak@hotmail.com>
2024-02-25 10:10:15 +0200

    Op Task 3

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -87,10 +87,10 @@ def delete_place(place_id):
 def places_search():
     """retrieves all Place objects
     depending of the JSON in the body of the request"""
-    data = request.get_json()
-    if not data:
+    if not request.is_json:
         return {'error': 'Not a JSON'}, 400
-    if data != {} and not all(map(lambda x: len(x) == 0, data.values())):
+    data = request.get_json()
+    if not data and not all(map(lambda x: len(x) == 0, data.values())):
         results = []
         states = data.get('states')
         cities = data.get('cities')
